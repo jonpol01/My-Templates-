0ac09f36f007b5afe5a15a5bb762a098b653f45d
jonpol01 <johnpaul@soliva.me>
2016-08-30 11:33:25 +0900

    0830

---
+++ Cpp-temp/Cpp-temp/main.hpp
@@ -0,0 +1,15 @@
+//
+//  node-list.hpp
+//  Cpp-temp
+//
+//  Created by John Paul Soliva on 8/30/16.
+//  Copyright © 2016 Soliva John Paul. All rights reserved.
+//
+
+#ifndef node_list_hpp
+#define node_list_hpp
+
+#include <iostream>
+#include <stdio.h>
+
+#endif /* node_list_hpp */
---
+++ Cpp-temp/Cpp-temp/node-list.cpp
@@ -0,0 +1,15 @@
+//
+//  node-list.cpp
+//  Cpp-temp
+//
+//  Created by John Paul Soliva on 8/30/16.
+//  Copyright © 2016 Soliva John Paul. All rights reserved.
+//
+
+#include "main.hpp"
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
